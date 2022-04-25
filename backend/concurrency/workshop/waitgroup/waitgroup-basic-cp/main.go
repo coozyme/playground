ea79ec39a848c86df1e19f40ce53616940b200d6
Ary Setya Pambudi <programmeranak@gmail.com>
2022-04-25 17:46:34 +0700

    update progress

--- backend/concurrency/workshop/waitgroup/waitgroup-basic-cp/main.go
+++ backend/concurrency/workshop/waitgroup/waitgroup-basic-cp/main.go
@@ -5,16 +5,24 @@ import "sync"
 func testWG(output chan<- []bool) {
 	// TODO: answer here
 
+	ch := make(chan int)
+	var wg sync.WaitGroup
 	done := make([]bool, 5)
 
 	for i := 0; i < 5; i++ {
+		count := 0
 		// TODO: answer here
+		wg.Add(1)
 		go func(i int) {
 			// TODO: answer here
+			defer wg.Done()
 			done[i] = true
+			count <- i
+
 		}(i)
 	}
 
 	// TODO: answer here
+	wg.Wait()
 	output <- done
 }
