5eebb6a1d7e0a938192c0a6a572670bf214ac01b
Ary Setya Pambudi <programmeranak@gmail.com>
2022-05-13 08:59:46 +0700

    update

--- backend/golang-http-server/default-http-server/client-cp/main.go
+++ backend/golang-http-server/default-http-server/client-cp/main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"strconv"
 )
 
 // Dari contoh yang diberikan, cobalah untuk mengimplementasikan sebuah http client sederhana.
@@ -13,4 +14,32 @@ import (
 
 func main() {
 	// TODO: answer here
+	var (
+		woeid, year, month, day int
+	)
+
+	woeid = 1047378
+	year = 2022
+	month = 05
+	day = 13
+
+	c := http.Client{}
+
+	resp, err := c.Get("https://www.metaweather.com/api/location/" + strconv.Itoa(woeid) + "/" + strconv.Itoa(year) + "/" + strconv.Itoa(month) + "/" + strconv.Itoa(day))
+
+	if err != nil {
+		fmt.Printf("Errpr: %s", err)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	body, err := ioutil.ReadAll(resp.Body)
+
+	if err != nil {
+		fmt.Printf("err : %s", err)
+		return
+	}
+	fmt.Println(string(body))
+
 }
