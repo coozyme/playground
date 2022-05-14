8e88402f0e08b015ec340835089a8fbd3f2d36a0
Ary Setya Pambudi <programmeranak@gmail.com>
2022-05-14 19:27:02 +0700

    update handler

--- backend/golang-http-server/handler/2-custom-handler-cp/main.go
+++ backend/golang-http-server/handler/2-custom-handler-cp/main.go
@@ -19,7 +19,11 @@ var quotes = []string{
 }
 
 // TODO: answer here
+type QuotesHandler struct{}
 
 func (qh QuotesHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	// TODO: answer here
+	randomIndex := rand.Intn(len(quotes))
+	pickedQuote := quotes[randomIndex]
+	w.Write([]byte(fmt.Sprintf("%s", pickedQuote)))
 }
