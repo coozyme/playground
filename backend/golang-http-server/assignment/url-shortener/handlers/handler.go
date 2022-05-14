0ca34cc572d8831fa5ac804671043b5f0f1cfa32
Ary Setya Pambudi <programmeranak@gmail.com>
2022-05-14 19:25:27 +0700

    update urlshorter handler

--- backend/golang-http-server/assignment/url-shortener/handlers/handler.go
+++ backend/golang-http-server/assignment/url-shortener/handlers/handler.go
@@ -3,6 +3,7 @@ package handlers
 import (
 	"net/http"
 
+	"github.com/ruang-guru/playground/backend/golang-http-server/assignment/url-shortener/entity"
 	"github.com/ruang-guru/playground/backend/golang-http-server/assignment/url-shortener/repository"
 
 	"github.com/gin-gonic/gin"
@@ -20,14 +21,50 @@ func NewURLHandler(repo *repository.URLRepository) URLHandler {
 
 func (h *URLHandler) Get(c *gin.Context) {
 	// TODO: answer here
-	c.Redirect(http.StatusFound, "/linkshorter")
+	shortURL := c.Param("shortURL")
+	entityURL, err := h.repo.Get(shortURL)
+	if err != nil {
+		c.JSON(http.StatusNotFound, gin.H{"message": err.Error()})
+		return
+	}
+
+	c.Writer.Header().Set("Location", entityURL.LongURL)
+	c.JSON(http.StatusFound, gin.H{
+		"message": "Redirecting...",
+	})
 }
 
 func (h *URLHandler) Create(c *gin.Context) {
 	// TODO: answer here
+	var entityURL entity.URL
+	errReq := c.ShouldBindJSON(&entityURL)
+	if errReq != nil {
+		c.JSON(http.StatusBadRequest, entity.ErrBadRequest)
+	}
+
+	entityURLResp, err := h.repo.Create(entityURL.LongURL)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"Data": entityURLResp,
+	})
 
 }
 
 func (h *URLHandler) CreateCustom(c *gin.Context) {
 	// TODO: answer here
+	var entityURL entity.URL
+	errReq := c.ShouldBindJSON(&entityURL)
+	if errReq != nil {
+		c.JSON(http.StatusBadRequest, entity.ErrBadRequest)
+	}
+
+	entityURLResp, err := h.repo.CreateCustom(entityURL.LongURL, entityURL.ShortURL)
+	if err != nil {
+		c.JSON(http.StatusBadRequest, err.Error())
+	}
+	c.JSON(http.StatusOK, gin.H{
+		"Data": entityURLResp,
+	})
 }
