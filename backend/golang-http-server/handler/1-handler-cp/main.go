2ca60380a52286b3570357d1bdb2b8f2cf6b4d5c
Ary Setya Pambudi <programmeranak@gmail.com>
2022-05-14 19:12:05 +0700

    update handler

--- backend/golang-http-server/handler/1-handler-cp/main.go
+++ backend/golang-http-server/handler/1-handler-cp/main.go
@@ -10,5 +10,12 @@ import (
 // Hint, gunakan time.Weekday, time.Day, time.Month, dan time.Year
 
 func GetHandler() http.HandlerFunc {
-	return func(writer http.ResponseWriter, request *http.Request) {} // TODO: replace this
+	// return func(writer http.ResponseWriter, request *http.Request) {} // TODO: replace this
+	t := time.Now()
+	test := fmt.Sprintf("%s, %d %s %d", t.Weekday(), t.Day(), t.Month(), t.Year())
+
+	var handler http.HandlerFunc = func(writer http.ResponseWriter, request *http.Request) {
+		writer.Write([]byte(test))
+	}
+	return handler
 }
