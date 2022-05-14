3908f47395e9832ee051ea899f97a3b83201f2b6
Ary Setya Pambudi <programmeranak@gmail.com>
2022-05-14 19:58:11 +0700

    4-query-string-cp

--- backend/golang-http-server/gin/4-query-string-cp/main.go
+++ backend/golang-http-server/gin/4-query-string-cp/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 
 	"github.com/gin-gonic/gin"
@@ -10,6 +11,24 @@ func GetRouter() *gin.Engine {
 	router := gin.Default()
 
 	// TODO: answer here
+	router.GET("/movie", func(ctx *gin.Context) {
+		genre := ctx.Query("genre")
+		country := ctx.Query("country")
+
+		// if condition {
+
+		// }
+		fmt.Println("DEB-", genre, country)
+		if len(country) > 1 && len(genre) > 1 {
+			ctx.String(http.StatusOK, "Here result of query of movie with genre %s in country %s", genre, country)
+		} else if len(country) > 1 {
+			ctx.String(http.StatusOK, "Here result of query of movie with genre general in country %s", country)
+		} else if len(genre) > 1 {
+			ctx.String(http.StatusOK, "Here result of query of movie with genre %s", genre)
+		} else {
+			ctx.String(http.StatusOK, "Here result of query of movie with genre general")
+		}
+	})
 
 	return router
 }
